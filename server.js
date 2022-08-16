8bedf5c387b1e212c3fcc0e6bee0016e5d8b166e
akshaymondal528 <akshaymondal528@gmail.com>
2022-08-16 22:34:24 +0530

    master: 16-08-2022 | auth module

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const path = require('path');
 // Local Imports
 const { CONST_CREDENTIALS } = require('./src/config/env');
 const { connectDB } = require('./src/database/db');
+const { apiRoute } = require('./src/routes');
+const { errorResponse } = require('./src/helpers/response');
+const { ERROR } = require('./src/helpers/constant');
 
 const app = express();
 
@@ -17,6 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cors());
+
+// Routes
+apiRoute(app);
+// Error route
+app.use('*', (req, res) => {
+    errorResponse(res, ERROR.PAGE_NOT_FOUND.success, ERROR.PAGE_NOT_FOUND.statuscode, ERROR.PAGE_NOT_FOUND.message);
+});
 
 const port = CONST_CREDENTIALS.PORT;
 app.listen(port, () => {
